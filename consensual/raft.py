7ba3a3e03f9bcc72da1a623514d6d62fe4416ddc
Azat Ibrakov <azatibrakov@gmail.com>
2022-01-02 14:06:38 +0300

    Extract implementation into 'core' package

---
+++ consensual/raft.py
@@ -0,0 +1,11 @@
+from .core.raft import (Node,
+                        NodeId,
+                        Record,
+                        Route,
+                        Term)
+
+Node = Node
+NodeId = NodeId
+Record = Record
+Route = Route
+Term = Term
