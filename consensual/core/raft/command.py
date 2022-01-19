35bfb90a0683a4a00ccbbb9700c43cad122ce1a4
Azat Ibrakov <azatibrakov@gmail.com>
2022-01-19 22:29:15 +0300

    Complete 'Command' class

--- consensual/core/raft/command.py
+++ consensual/core/raft/command.py
@@ -1,12 +1,36 @@
-import dataclasses
 from typing import (Any,
+                    Dict,
                     Generic,
                     TypeVar)
+
+from reprit.base import generate_repr
 
 _T = TypeVar('_T')
 
 
-@dataclasses.dataclass(frozen=True)
 class Command(Generic[_T]):
-    action: str
-    parameters: Any
+    __slots__ = '_action', '_parameters'
+
+    def __init__(self,
+                 *,
+                 action: str,
+                 parameters: Any) -> None:
+        self._action, self._parameters = action, parameters
+
+    __repr__ = generate_repr(__init__)
+
+    @property
+    def action(self) -> str:
+        return self._action
+
+    @property
+    def parameters(self) -> Any:
+        return self._parameters
+
+    @classmethod
+    def from_json(cls, *, action: str, parameters: Any) -> 'Command':
+        return cls(action=action,
+                   parameters=parameters)
+
+    def as_json(self) -> Dict[str, Any]:
+        return {'action': self.action, 'parameters': self.parameters}
