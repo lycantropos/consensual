a0b1d57dc1b0766104e0ab1da8396e7b9f1236ee
Azat Ibrakov <azatibrakov@gmail.com>
2022-01-20 03:27:23 +0300

    Complete 'Command' class

--- consensual/core/raft/command.py
+++ consensual/core/raft/command.py
@@ -11,13 +11,15 @@ _T = TypeVar('_T')
 class Command(Generic[_T]):
     __slots__ = '_action', '_parameters'
 
-    def __init__(self,
-                 *,
-                 action: str,
-                 parameters: Any) -> None:
+    def __new__(cls,
+                *,
+                action: str,
+                parameters: Any) -> 'Command':
+        self = super().__new__(cls)
         self._action, self._parameters = action, parameters
+        return self
 
-    __repr__ = generate_repr(__init__)
+    __repr__ = generate_repr(__new__)
 
     @property
     def action(self) -> str:
@@ -27,10 +29,7 @@ class Command(Generic[_T]):
     def parameters(self) -> Any:
         return self._parameters
 
-    @classmethod
-    def from_json(cls, *, action: str, parameters: Any) -> 'Command':
-        return cls(action=action,
-                   parameters=parameters)
+    from_json = classmethod(__new__)
 
     def as_json(self) -> Dict[str, Any]:
         return {'action': self.action, 'parameters': self.parameters}
