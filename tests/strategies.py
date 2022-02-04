2f597eddb569792e4e20d9253be7e56757e73ef6
Azat Ibrakov <azatibrakov@gmail.com>
2022-02-04 16:37:03 +0300

    Complete strategies

--- tests/strategies.py
+++ tests/strategies.py
@@ -16,8 +16,8 @@ from consensual.raft import (Node,
 from .running_node import RunningNode
 from .utils import MAX_RUNNING_NODES_COUNT
 
-heartbeats = strategies.floats(2, 4)
-delays = strategies.floats(0, 2)
+heartbeats = strategies.floats(1, 2)
+delays = strategies.floats(2, 4)
 hosts = strategies.just('localhost')
 ports = strategies.integers(4000, 5000)
 cluster_urls = strategies.builds(URL.build,
