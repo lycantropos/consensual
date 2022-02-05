8379f92e6dfcc4c1219d22622d8f2990420e64b2
Azat Ibrakov <azatibrakov@gmail.com>
2022-02-05 06:11:11 +0300

    Complete tests configurations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,12 @@
 import os
+from datetime import timedelta
 
 from hypothesis import settings
 
 on_azure_pipelines = bool(os.getenv('TF_BUILD', False))
+max_examples = (settings.default.max_examples // 5
+                if on_azure_pipelines
+                else settings.default.max_examples)
 settings.register_profile('default',
-                          max_examples=(settings.default.max_examples // 5
-                                        if on_azure_pipelines
-                                        else settings.default.max_examples),
-                          deadline=None)
+                          deadline=timedelta(hours=1) / max_examples,
+                          max_examples=max_examples)
