4ef4e87db20a388f8afa24084f310d198ce424da
Azat Ibrakov <azatibrakov@gmail.com>
2022-02-20 08:13:29 +0300

    Complete API

--- consensual/raft.py
+++ consensual/raft.py
@@ -1,9 +1,11 @@
 from .core.raft import (CallKey,
                         Node,
                         Processor,
-                        Receiver)
+                        Receiver,
+                        aiohttp)
 
 CallKey = CallKey
 Node = Node
 Processor = Processor
 Receiver = Receiver
+aiohttp = aiohttp
