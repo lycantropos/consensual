1b8fa8ac53cd6e603768cd86664dd94e297f3752
Azat Ibrakov <azatibrakov@gmail.com>
2022-02-21 14:32:16 +0300

    Complete API

--- consensual/raft.py
+++ consensual/raft.py
@@ -2,10 +2,14 @@ from .core.raft import (MessageKind,
                         Node,
                         Processor,
                         Receiver,
+                        ReceiverUnavailable,
+                        Sender,
                         aiohttp)
 
 MessageKind = MessageKind
 Node = Node
 Processor = Processor
 Receiver = Receiver
+ReceiverUnavailable = ReceiverUnavailable
+Sender = Sender
 aiohttp = aiohttp
