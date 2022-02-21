5f73cced917878c52da450ed160ff4d887a24d34
Azat Ibrakov <azatibrakov@gmail.com>
2022-02-21 20:24:18 +0300

    Complete 'Receiver' interface

--- consensual/core/raft/receiver.py
+++ consensual/core/raft/receiver.py
@@ -10,6 +10,15 @@ class Receiver(ABC):
     def from_node(cls, node: Node) -> 'Receiver':
         """Constructs receiver from given node."""
 
+    @property
+    @abstractmethod
+    def is_running(self) -> bool:
+        """Checks whether receiver is running."""
+
     @abstractmethod
     def start(self) -> None:
         """Starts receiving messages."""
+
+    @abstractmethod
+    def stop(self) -> None:
+        """Stops receiving messages."""
