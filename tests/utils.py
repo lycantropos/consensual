8b97348e425121d6a7d621925cf27145762dd951
Azat Ibrakov <azatibrakov@gmail.com>
2022-02-22 12:52:51 +0300

    Complete tests utilties

--- tests/utils.py
+++ tests/utils.py
@@ -6,6 +6,10 @@ from typing import (List,
 MAX_RUNNING_NODES_COUNT = 10
 
 
+def equivalence(left: bool, right: bool) -> bool:
+    return left is right
+
+
 def implication(antecedent: bool, consequent: bool) -> bool:
     return not antecedent or consequent
 
