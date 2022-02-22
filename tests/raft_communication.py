0cbd1013016d16751d053d3cc49f89de8a923778
Azat Ibrakov <azatibrakov@gmail.com>
2022-02-22 17:44:45 +0300

    Complete 'RaftCommunication' class

--- tests/raft_communication.py
+++ tests/raft_communication.py
@@ -1,61 +1,13 @@
-from typing import (Any,
-                    Collection,
-                    Mapping,
+from typing import (Collection,
                     MutableMapping)
 
 from reprit.base import generate_repr
 from yarl import URL
 
-from consensual.raft import (MessageKind,
-                             Node,
+from consensual.raft import (Node,
                              Receiver,
-                             ReceiverUnavailable,
-                             Sender)
-
-
-class RaftReceiver(Receiver):
-    def __init__(self, node: Node, nodes: MutableMapping[str, Node]) -> None:
-        self.node, self.nodes = node, nodes
-        self._is_running = False
-
-    __repr__ = generate_repr(__init__)
-
-    @property
-    def is_running(self) -> bool:
-        return self._is_running
-
-    def start(self) -> None:
-        if self._is_running:
-            raise RuntimeError('is already running')
-        result = self.nodes.setdefault(self.node.url.authority, self.node)
-        if result is not self.node:
-            raise OSError('already exists')
-        else:
-            self._is_running = True
-
-    def stop(self) -> None:
-        if self.is_running:
-            del self.nodes[self.node.url.authority]
-            self._is_running = False
-
-
-class RaftSender(Sender):
-    def __init__(self, urls: Collection[URL], nodes: Mapping[str, Node]
-                 ) -> None:
-        self.nodes, self.urls = nodes, urls
-
-    __repr__ = generate_repr(__init__)
-
-    async def send(self, *, kind: MessageKind, message: Any, url: URL
-                   ) -> Any:
-        if url not in self.urls:
-            raise ReceiverUnavailable(url)
-        try:
-            receiver = self.nodes[url.authority]
-        except KeyError as exception:
-            raise ReceiverUnavailable(url) from exception
-        return await receiver.receive(kind=kind,
-                                      message=message)
+                             Sender,
+                             plain)
 
 
 class RaftCommunication:
@@ -65,7 +17,7 @@ class RaftCommunication:
     __repr__ = generate_repr(__init__)
 
     def to_receiver(self, node: Node) -> Receiver:
-        return RaftReceiver(node, self.nodes)
+        return plain.Receiver(node, self.nodes)
 
     def to_sender(self, urls: Collection[URL]) -> Sender:
-        return RaftSender(urls, self.nodes)
+        return plain.Sender(urls, self.nodes)
