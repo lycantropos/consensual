85e3c06273a7751d67de54b3cf19b7f14322ca94
Azat Ibrakov <azatibrakov@gmail.com>
2022-02-22 17:37:19 +0300

    Complete synchronization

--- consensual/core/raft/node.py
+++ consensual/core/raft/node.py
@@ -363,7 +363,9 @@ class Node:
         self._restart_reelection_timer()
         if call.term > self._role.term:
             self._withdraw(call.term)
-        if call.term == self._role.term and self._id != call.node_id:
+        if (call.term == self._role.term
+                and self._role.leader_node_id is None
+                and self._id != call.node_id):
             self._follow(call.node_id)
         states_agree = (
                 call.term == self._role.term
